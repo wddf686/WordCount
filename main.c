01fc532c17741a717e352c026c1bc5f15dabe856
XML <2825992858@qq.com>
2024-05-18 15:31:37 +0800

    add code

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <ctype.h>  
+  
+int main(int argc, char *argv[]) {  
+    char *param = NULL, *filename = NULL;  
+    FILE *file = NULL;  
+    int ch, word_count = 0, char_count = 0;  
+    int in_word = 0;   
+  
+    if (argc != 3) {  
+        fprintf(stderr, "Usage: %s [-c|-w] [input_file_name]\n", argv[0]);  
+        return 1;  
+    }  
+  
+    param = argv[1];  
+    filename = argv[2];  
+  
+    if (strcmp(param, "-c") != 0 && strcmp(param, "-w") != 0) {  
+        fprintf(stderr, "Invalid parameter: %s\n", param);  
+        return 1;  
+    }  
+  
+    file = fopen(filename, "r");  
+    if (file == NULL) {  
+        perror(filename);  
+        return 1;  
+    }  
+  
+    while ((ch = fgetc(file)) != EOF) {  
+        char_count++;  
+  
+        if (isspace(ch) || ch == ',') {  
+            in_word = 0;  
+        } else if (!in_word) {  
+            word_count++;  
+            in_word = 1;  
+        }  
+    }  
+  
+    if (param[0] == '-' && param[1] == 'c') {  
+        printf("×Ö·ûÊý£º%d\n", char_count);  
+    } else if (param[0] == '-' && param[1] == 'w') {  
+        printf("µ¥´ÊÊý£º%d\n", word_count);  
+    }  
+  
+    fclose(file);  
+    return 0;  
+}
